831ff3452a45ddecbc9bc87694402348e560a7f5
Malissa Cruz Romero <romero.malissa@gmail.com>
2015-09-15 12:55:14 -0400

    updated user.js in services

--- app/assets/javascripts/users/services/user.js
+++ app/assets/javascripts/users/services/user.js
@@ -1,8 +1,8 @@
 (function(){
   var userServices = angular.module('userServices', ['ngResource']);
   userServices.factory('User', ['$resource', function($resource){
-    // return $resource('http://localhost:3000/users/:id', {}, {
-    //   update: {method: 'PUT'}
-    // })
+    return $resource('http://localhost:3000/users/:id', {}, {
+      update: {method: 'PUT'}
+    })
   }])
 })();
